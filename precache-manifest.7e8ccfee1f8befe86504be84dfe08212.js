7c887e87269aee6854ed2b9fca93f1cac63e0c22
Mustafiz <js.mustafiz@gmail.com>
2020-08-31 09:45:29 +0600

    Updates

---
+++ precache-manifest.7e8ccfee1f8befe86504be84dfe08212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76669f000624064a31b726e6404d0e20",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "10b571d5042d1937cc6e",
+    "url": "/amazon-clone/static/css/main.759bd08c.chunk.css"
+  },
+  {
+    "revision": "63c6445d6e75d5b17b80",
+    "url": "/amazon-clone/static/js/2.667a43da.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon-clone/static/js/2.667a43da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b571d5042d1937cc6e",
+    "url": "/amazon-clone/static/js/main.479958a1.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
