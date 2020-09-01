9086c665a6d7c6bdc316d64cf967003d67f66b95
Mustafiz <js.mustafiz@gmail.com>
2020-09-01 15:47:30 +0600

    Add-to-card bug fixed

--- src/Home.js
+++ src/Home.js
@@ -1,60 +1,65 @@
-import React from 'react'
-import './home.css'
-import Product from './Product'
+import React from "react";
+import "./home.css";
+import Product from "./Product";
 
 function Home() {
-    return (
-        <div className="home">
-            <img className="home_image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg" />
+  return (
+    <div className="home">
+      <img
+        className="home_image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg"
+      />
 
-            {/* product id, title, price, rating, image */}
-            <div className="home_row">
-                <Product
-                    id="1612020100"
-                    title="Paper Craft: lorem external -content. duckduckgo.com"
-                    price="11.26"
-                    rating={5}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Lod0StyO1YKop-XnMT3a4AHaL0%26pid%3DApi&f=1"
-                />
-                <Product
-                    id="1612020103"
-                    title="Iphone X: lorem external -content. duckduckgo.com"
-                    price="12.26"
-                    rating={4}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dhD58VU4PaEln-tnmc-WPgHaHa%26pid%3DApi&f=1"
-                />
-            </div>
+      {/* product id, key, title, price, rating, image */}
+      <div className="home_row">
+        <Product
+          id={1612020100}
+          key={1}
+          title="Paper Craft: lorem external -content. duckduckgo.com"
+          price={11.26}
+          rating={5}
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Lod0StyO1YKop-XnMT3a4AHaL0%26pid%3DApi&f=1"
+        />
+        <Product
+          id={1612020103}
+          key={2}
+          title="Iphone X: lorem external -content. duckduckgo.com"
+          price={12.26}
+          rating={4}
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dhD58VU4PaEln-tnmc-WPgHaHa%26pid%3DApi&f=1"
+        />
+      </div>
 
-            <div className="home_row">
-                <Product
-                    id="1712020106"
-                    title="Macbook Cover: lorem external -content. duckduckgo.com"
-                    price="13.26"
-                    rating={3}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zeklw2xSB79jyigO_rUCkwHaHa%26pid%3DApi&f=1"
-                />
-                <Product
-                    id="1812020100"
-                    title="Canon 70D: lorem external -content. duckduckgo.com"
-                    price="14.26"
-                    rating={5}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.UbpuCUpvp0swFPi1-3ESJwHaFE%26pid%3DApi&f=1"
-                />
-                <Product
-                    id="2112020109"
-                    title="Samsun Gear 3: lorem external -content. duckduckgo.com"
-                    price="15.26"
-                    rating={4}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.iOXhw59wZSKm0JEholQ4IQHaE9%26pid%3DApi&f=1"
-                />
-            </div>
+      <div className="home_row">
+        <Product
+          id={1712020106}
+          key={3}
+          title="Macbook Cover: lorem external -content. duckduckgo.com"
+          price={13.26}
+          rating={3}
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zeklw2xSB79jyigO_rUCkwHaHa%26pid%3DApi&f=1"
+        />
+        <Product
+          id={1812020100}
+          key={4}
+          title="Canon 70D: lorem external -content. duckduckgo.com"
+          price={14.26}
+          rating={5}
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.UbpuCUpvp0swFPi1-3ESJwHaFE%26pid%3DApi&f=1"
+        />
+        <Product
+          id={2112020109}
+          key={5}
+          title="Samsun Gear 3: lorem external -content. duckduckgo.com"
+          price={15.26}
+          rating={4}
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.iOXhw59wZSKm0JEholQ4IQHaE9%26pid%3DApi&f=1"
+        />
+      </div>
 
-
-            {/* Product  */}
-        </div>
-
-
-    )
+      {/* Product  */}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/Product.js
+++ src/Product.js
@@ -1,46 +1,49 @@
-import React from 'react'
-import './product.css'
-import { useStateValue } from './StateProvider';
+import React from "react";
+import "./product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ id, title, price, rating, image }) {
+function Product(props) {
+  const [{}, dispatch] = useStateValue();
+  console.log(props);
 
-    const [{ }, dispatch] = useStateValue();
+  const { id, key, title, price, rating, image } = props;
+  //   const addToBasket = () => {
+  //     dispatch({
+  //       type: "ADD_TO_BASKET",
+  //       item: {
+  //         id: id,
+  //         key: key,
+  //         title: title,
+  //         image: image,
+  //         price: price,
+  //         rating: rating,
+  //       },
+  //     });
+  //   };
 
-    const addToBasket = () => {
-        dispatch({
-            type: 'ADD_TO_BASKET',
-            item: {
-                id: id,
-                title: title,
-                image: image,
-                price: price,
-                rating: rating
-            }
-        })
-    }
+  return (
+    <div className="product">
+      <div className="product_info">
+        <p>{title}</p>
 
-    return (
-        <div className="product">
-            <div className="product_info">
-                <p>{title}</p>
+        <p className="product_price">
+          <small> $ </small>
+          <strong> {price} </strong>
+        </p>
 
-                <p className="product_price">
-                    <small> $ </small>
-                    <strong> {price} </strong>
-                </p>
-
-                <div className="product_rating">
-                    {
-                        Array(rating).fill().map((_) =>
-                            <p>★</p>)
-                    }
-                </div>
-            </div>
-
-            <img src={image} alt="" />
-            <button onClick={addToBasket}>Add to Basket</button>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>★</p>
+            ))}
         </div>
-    )
+      </div>
+
+      <img src={image} alt="" />
+      {/* <button onClick={addToBasket}>Add to Basket</button> */}
+    </div>
+  );
 }
 
-export default Product
+export default Product;
